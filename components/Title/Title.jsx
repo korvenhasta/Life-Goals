196b9cb2883a1f668f0a3c1df429eec27589df14
Marina Kenina <avrora-lisa@yandex.ru>
2023-01-02 10:53:36 +0100

    made TaskCard with mobile/desktop implementation

---
+++ components/Title/Title.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Title.module.css'
+
+export default function Title({children, ...props}) {
+  return (
+    <h1 {...props} className={styles.title}>
+      {children}
+    </h1>
+  )
+}
