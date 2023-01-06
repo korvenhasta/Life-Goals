d5e68747dc41de80d3497b772ff7381b71ae05a6
Marina Kenina <avrora-lisa@yandex.ru>
2023-01-06 12:06:14 +0100

    changed button colors and visual presentation

--- components/TaskCard/TaskCard.jsx
+++ components/TaskCard/TaskCard.jsx
@@ -4,6 +4,7 @@ import Title from '../Title/Title'
 import Paragraph from '../Paragraph/Paragraph'
 import ButtonContainer from '../ButtonContainer/ButtonContainer'
 import Button from '../Button/Button'
+import Container from '../Container/Container'
 
 export default function TaskCard({...props}) {
   return (
@@ -11,9 +12,11 @@ export default function TaskCard({...props}) {
       <Title>{props.title}</Title>
       <Paragraph>Estimated due date: {props.date}</Paragraph>
       <ButtonContainer>
-        <Button>Edit</Button>
-        <Button variant={"button_close"}>Close</Button>
-        <Button variant={"button_delete"}>Delete</Button>
+        <Button>Complete</Button>
+        <Container>
+          <Button variant={"button_edit"}>Edit</Button>
+          <Button variant={"button_delete"}>Delete</Button>
+        </Container>
       </ButtonContainer>
     </div>
   )
