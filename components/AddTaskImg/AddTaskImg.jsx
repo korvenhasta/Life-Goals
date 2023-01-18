8bd8a17f807a91845ac39c199e82114ee7fbe456
Marina Kenina <avrora-lisa@yandex.ru>
2023-01-18 16:24:33 +0100

    made a desktop version AddTask

--- components/AddTaskImg/AddTaskImg.jsx
+++ components/AddTaskImg/AddTaskImg.jsx
@@ -1,15 +1,18 @@
 import Image from 'next/image'
 import React from 'react'
+import ImageContainer from '../ImageContainer/ImageContainer'
 import styles from './AddTaskImg.module.css'
 
 export default function AddTaskImg() {
   return (
-    <Image
-      src="/addtask.svg"
-      alt="Picture of the adding a new task"
-      width={40}
-      height={40}
-      className={styles.addTaskImg}
-    />
+    <ImageContainer>
+      <Image
+        src="/addtask.svg"
+        alt="Picture of the adding a new task"
+        className={styles.addTaskImg}
+        fill
+        sizes="(min-width: 768px) 100vw"
+      />
+    </ImageContainer>
   )
 }
---
+++ components/ImageContainer/ImageContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './ImageContainer.module.css'
+
+export default function ImageContainer({
+  children, 
+  ...props
+}
+) {
+  return (
+    <div {...props} 
+    className={styles.imageContainer}
+    >
+      {children}
+    </div>
+  )
+}
